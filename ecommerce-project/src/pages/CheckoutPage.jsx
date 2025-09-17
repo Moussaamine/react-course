2890396e8138792247b8a326aee0825eeac0f3d0
moussa <amin.moussa2004@gmail.com>
2025-09-17 19:55:59 +0100

    Split header into a component and use Link

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -1,10 +1,11 @@
-import './checkout-header.css'
 import './CheckoutPage.css'
+import './CheckoutPage-header.css'
 
 export function CheckoutPage() {
     return (
         <>
             <title>Checkout</title>
+            
             <div className="checkout-header">
                 <div className="header-content">
                     <div className="checkout-header-left-section">
